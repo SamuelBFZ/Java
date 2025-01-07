96780c961af6bc3b8a676aeae5b1adde5b952723
SamuelBFZ <157266433+SamuelBFZ@users.noreply.github.com>
2025-01-06 19:07:33 -0500

    Updated constructor from Cuadrilatero.java

--- POO/Package/Cuadrilatero.java
+++ POO/Package/Cuadrilatero.java
@@ -6,9 +6,9 @@ public class Cuadrilatero {
     private float lado2;
 
     //constructor
-    public Cuadrilatero(float _lado1, float _lado2) {
-        lado1 = _lado1;
-        lado2 = _lado2;
+    public Cuadrilatero(float lado1, float lado2) {
+        this.lado1 = lado1;
+        this.lado2 = lado2;
     }
 
     public Cuadrilatero(float _lado1){
